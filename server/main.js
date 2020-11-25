e53b40101750cef862f4b7bab00a5fa10459bf34
intertext.no <orstavik77@gmail.com>
2020-11-25 18:44:50 +0100

    first test

---
+++ server/main.js
@@ -0,0 +1,23 @@
+//ROOT (directory. DO NOT END WITH '/'!!)
+function mimeType(path){
+  const filetype = path.substr(path.lastIndexOf('.')+1);
+  return {
+    'html': 'text/html',
+    'js': 'application/js',
+    'css': 'text/css'
+  }[filetype];
+}
+
+async function handleRequest(req) {
+  const url = new URL(req.url);
+  let path = url.pathname;
+  if (path === '' || path === '/')
+    path = '/index.html';
+  const headers = {'content-type': mimeType(path)};
+  const rawgit = await fetch(ROOT + path, {cf: {cacheTtl: -1}});//do not cache
+  if(rawgit.status === 200)
+    return new Response(await rawgit.text(), {status: 200, headers});
+  return rawgit;
+}
+
+addEventListener('fetch', e => e.respondWith(handleRequest(e.request)));
